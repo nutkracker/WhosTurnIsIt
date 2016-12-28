5b33920e4bf6554575799ad7fe3164cf18579646
Nutkracker <nutkracker@gmail.com>
2016-12-28 17:41:40 +0000

    updates

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = new alexa.app( 'WhosTurnIsIt' );
 
 
 app.launch( function( request, response ) {
-	response.say( 'Welcome to Who''s Turn Is It.' ).reprompt( 'Ready.' ).shouldEndSession( false );
+	response.say( 'Welcome to Who\'s Turn Is It.' ).reprompt( 'Ready.' ).shouldEndSession( false );
 } );
 
 
@@ -26,7 +26,7 @@ app.intent('sayWhoMakesCuppa',
   },
   function(request,response) {
     var intentType = request.slot('intentType');
-    response.say("You asked for "+intentType+".");
+    response.say("You asked for "+intentType);
     response.say("It's Andrea's turn.")
   }
 );
